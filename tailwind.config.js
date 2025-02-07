15152238a22c2260f41fd162d23dcb673d8de8a7
Sudeep <sudeepavirupapur@gmail.com>
2025-02-07 17:54:41 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    backgroundImage: {
+      "hero-pattern": "url('bg.jpg')",
+      extend: {},
+    },
+    plugins: [],
+  },
+};
